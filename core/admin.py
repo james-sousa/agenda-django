ebe37357cfb5ad0138e66470cfbeb3fbf0331ce7
James de Sousa <69591985+james-sousa@users.noreply.github.com>
2023-09-15 21:55:15 -0300

    Delete core directory

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from core.models import Evento
-# Register your models here.
-
-class EventoAdmin(admin.ModelAdmin):
-    list_display = ('id','titulo', 'data_evento', 'data_criacao')
-
-admin.site.register(Evento,EventoAdmin)
